fa6c8360aa08d4913fb8089ebc626c4bb2c3e326
ChaseSansom <13357045+ChaseSansom@users.noreply.github.com>
2025-03-12 08:51:12 -0700

    Initial Vapor project setup

---
+++ Sources/App/Models/CreateUser.swift
@@ -0,0 +1,16 @@
+import Fluent
+
+struct CreateUser: Migration {
+    func prepare(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("users")
+            .id()
+            .field("email", .string, .required)
+            .field("password_hash", .string, .required)
+            .unique(on: "email")
+            .create()
+    }
+    
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("users").delete()
+    }
+}
