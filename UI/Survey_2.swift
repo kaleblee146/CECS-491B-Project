c6a886704d19a2381276b7eb10ca2d112b69b33b
kaleblee146 <80921315+kaleblee146@users.noreply.github.com>
2025-03-19 00:44:56 -0700

    Update Survey_2.swift

--- UI/Survey_2.swift
+++ UI/Survey_2.swift
@@ -18,25 +18,44 @@ struct SurveyView2: View {
         NavigationStack{
             VStack{
                 Text("What's your age")
+                    .font(Font.custom("Roboto_condensed-black", size: 18))
+                    .padding(.bottom, 50)
                 Picker("", selection: $selectedAge){
                     ForEach(0..<100, id: \.self) {
                         age in
                         Text("\(age)").tag(age)
                     }
                 }
+                .pickerStyle(.menu)
+                //pickerStyle(.wheel)
+                
                 HStack{
                     Button("BACK"){
                         goBack = true
                     }
+                    .font(Font.custom("Roboto_Condensed-Black", size: 18))
+                    .frame(width: 164, height: 60)
                     .background(Color.blueMagenta)
+                    .foregroundColor(.white)
+                    .cornerRadius(15)
+                    .buttonStyle(BorderlessButtonStyle())
                     .padding()
+                    
+
                     
                     Button("CONTINUE"){
                         continueButton = true
                     }
+                    .font(Font.custom("Roboto_Condensed-Black", size: 18))
+                    .frame(width: 164, height: 60)
                     .background(Color.magenta)
+                    .foregroundColor(.white)
+                    .cornerRadius(15)
+                    .buttonStyle(BorderlessButtonStyle())
+                    .padding()
                 }
-                .padding(.top, 15)
+                .padding(.top, 250)
+                
             }
             .frame(width: 402, height: 869)
             .background(Color.navy)
