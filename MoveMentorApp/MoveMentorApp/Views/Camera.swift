f9d5c60d03a51ce1a5368beaa5f5d141135f8834
Sebastian (Salvador) Sandoval <102843169+SebastianSSandoval@users.noreply.github.com>
2025-04-08 23:42:42 -0700

    Created Camera.Swift
    Created Camera.,Swift as a simple ui for the Camera

---
+++ MoveMentorApp/MoveMentorApp/Views/Camera.swift
@@ -0,0 +1,23 @@
+//
+//  Camera.swift
+//  MoveMentorApp
+//
+//  Created by Sebs on 4/8/25./Users/sebs/Documents/GitHub/CECS-491B-Project/MoveMentorApp/MoveMentorApp.xcodeproj
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View{
+        VStack {
+            Text("Hellow World !")
+                .font(.largeTitle)
+                .padding()
+
+        }
+    }
+}
+
+#Preview{
+    ContentView()
+}
