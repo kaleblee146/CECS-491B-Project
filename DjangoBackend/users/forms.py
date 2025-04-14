204a27f9f24e436e34412c1dbee4794676d29dfd
ChaseSansom <13357045+ChaseSansom@users.noreply.github.com>
2025-04-13 22:42:43 -0700

    Updated forms.py, register.html

--- DjangoBackend/users/forms.py
+++ DjangoBackend/users/forms.py
@@ -1,4 +1,3 @@
-# users/forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
@@ -11,16 +10,35 @@ class CustomUserCreationForm(UserCreationForm):
         fields = [
             "username",
             "email",
-            "firstName",
-            "lastName",
-            "dob",
             "phone",
-            "gender",
-            "units",
-            "weight",
-            "height",
-            "goals",
-            "bio",
-            "profile_picture",
-            "role",
+            "dob",         # matches your model
+            "password1",
+            "password2",
         ]
+        widgets = {
+            "username": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Username",
+            }),
+            "email": forms.EmailInput(attrs={
+                "class": "form-control",
+                "placeholder": "Email",
+            }),
+            "phone": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Phone",
+            }),
+            "dob": forms.DateInput(attrs={
+                "class": "form-control",
+                "placeholder": "Date of birth",
+                "type": "date",
+            }),
+            "password1": forms.PasswordInput(attrs={
+                "class": "form-control",
+                "placeholder": "Password",
+            }),
+            "password2": forms.PasswordInput(attrs={
+                "class": "form-control",
+                "placeholder": "Confirm Password",
+            }),
+        }
