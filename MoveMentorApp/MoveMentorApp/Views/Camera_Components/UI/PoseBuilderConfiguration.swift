f9c799adbdef97ac273b303e247e5697ddd51c1e
Sebastian (Salvador) Sandoval <seabasssandoval@gmail.com>
2025-04-28 05:54:39 -0700

    Update PoseBuilderConfiguration.swift

--- MoveMentorApp/MoveMentorApp/Views/Camera_Components/UI/PoseBuilderConfiguration.swift
+++ MoveMentorApp/MoveMentorApp/Views/Camera_Components/UI/PoseBuilderConfiguration.swift
@@ -4,4 +4,9 @@ struct PoseBuilderConfiguration {
     var algorithm: Algorithm = .single
     var jointConfidenceThreshold: Double = 0.5
     var poseConfidenceThreshold: Double = 0.5
+    
+    // These are important for multiple-person pose estimation (calibration tuning)
+    var localSearchRadius: Int = 3
+    var matchingJointDistance: Float = 15.0
+    var adjacentJointOffsetRefinementSteps: Int = 5
 }
